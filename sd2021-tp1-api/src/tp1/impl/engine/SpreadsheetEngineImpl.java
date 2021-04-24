488b00cc80587f9fde8400f568a5e12901a1dee8
Francisco Silva <fapb.silva@campus.fct.unl.pt>
2021-04-24 21:18:45 +0100

    WHo knows

--- sd2021-tp1-api/src/tp1/impl/engine/SpreadsheetEngineImpl.java
+++ sd2021-tp1-api/src/tp1/impl/engine/SpreadsheetEngineImpl.java
@@ -13,8 +13,38 @@ import tp1.api.engine.SpreadsheetEngine;
 import tp1.util.CellRange;
 
 
+/**
+Example of use:
+Spreadsheet sheet = ...
+String[][] values = SpreadsheetEngineImpl.getInstance().computeSpreadsheetValues(new AbstractSpreadsheet() {
+	@Override
+	public int rows() {
+		return sheet.getRows();
+	}
+	@Override
+	public int columns() {
+		return sheet.getColumns();
+	}
+	@Override
+	public String sheetId() {
+		return sheet.getSheetId();
+	}
+	@Override
+	public String cellRawValue(int row, int col) {
+		try {
+			return sheet.getRawValues()[row][col];
+		} catch (IndexOutOfBoundsException e) {
+			return "#ERROR?";
+		}
+	}
+	@Override
+	public String[][] getRangeValues(String sheetURL, String range) {
+		// get remote range values
+	});
+*/
 public class SpreadsheetEngineImpl implements SpreadsheetEngine {
 	
+	private static final String ERROR = "#ERROR?";
 	private SpreadsheetEngineImpl() {		
 	}
 
@@ -33,14 +63,20 @@ public class SpreadsheetEngineImpl implements SpreadsheetEngine {
 				ExcelCell cell = worksheet.getCell(i, j);
 				setCell(sheet, worksheet, cell, rawVal);
 			}
-		
+
+//		try {
+//			workbook.save("/tmp/" + sheet.sheetId() + ".xls");
+//		} catch( Exception x ) {
+//			x.printStackTrace();
+//		}
 		worksheet.calculate();
 
 		var cells = new String[sheet.rows()][sheet.columns()];
 		for (int row = 0; row < sheet.rows(); row++) {
 			for (int col = 0; col < sheet.columns(); col++) {
 				ExcelCell cell = worksheet.getCell(row, col);
-				cells[row][col] = cell.getValue().toString();
+				var value = cell.getValue();
+				cells[row][col] = value != null ? value.toString() : ERROR;
 			}
 		}
 		return cells;
@@ -62,6 +98,7 @@ public class SpreadsheetEngineImpl implements SpreadsheetEngine {
 				cell.setFormula(rawVal);
 			break;
 		case TEXT:
+		case EMPTY:
 			cell.setValue(rawVal);
 		break;
 		case IMPORTRANGE:
@@ -69,10 +106,12 @@ public class SpreadsheetEngineImpl implements SpreadsheetEngine {
 			if( matcher.matches()) {
 				var sheetUrl = matcher.group(1);
 				var range = matcher.group(2);
-				var values = sheet.getRangeValues(sheetUrl, range);		
-				applyRange( worksheet, cell, new CellRange(range), values);
+				var values = sheet.getRangeValues(sheetUrl, range);
+				if( values != null )
+					applyRange( worksheet, cell, new CellRange(range), values);
+				else
+					cell.setValue(ERROR);
 			}
-		case EMPTY:
 			break;
 		};
 	}
